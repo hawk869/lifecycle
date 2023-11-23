f63b39324227930a29b66a7ad91fc4d6eb13deb7
Daniel Esteban <decepeda@misena.edu.co>
2023-11-22 20:24:33 -0500

    Proyecto para lifecycle en angular

---
+++ src/app/products/components/price/price.component.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'products-price',
+  templateUrl: './price.component.html',
+  styleUrls: ['./price.component.css']
+})
+export class PriceComponent { }
---
+++ src/app/products/pages/product/product.component.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'products-product-page',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent { }
